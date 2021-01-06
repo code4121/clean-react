1c0c0786720c36830fb2cb58726acf157feb806d
Diego Pupato <pupato.diego@gmail.com>
2021-01-07 12:28:17 +1300

    test: ensure Login starts with inputs with required state

--- src/presentation/pages/login/login.spec.tsx
+++ src/presentation/pages/login/login.spec.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 import { render } from "@testing-library/react";
+import { IconBaseProps } from "react-icons";
+import { FaExclamationCircle } from "react-icons/fa";
 import Login from "./login";
 
 describe("Login Component", () => {
@@ -8,8 +10,18 @@ describe("Login Component", () => {
 
         const errorWrap = getByTestId("error-wrap");
         const submitButton = getByTestId("submit") as HTMLButtonElement;
+        const emailStatus = getByTestId("email-status");
+        const emailErrorIcon = getByTestId("email-error-icon") as IconBaseProps;
+        const passwordStatus = getByTestId("password-status");
+        const passwordErrorIcon = getByTestId(
+            "password-error-icon",
+        ) as IconBaseProps;
 
         expect(errorWrap.childElementCount).toBe(0);
         expect(submitButton.disabled).toBe(true);
+        expect(emailStatus.title).toBe("Required field");
+        expect(emailErrorIcon).toBeTruthy();
+        expect(passwordStatus.title).toBe("Required field");
+        expect(passwordErrorIcon).toBeTruthy();
     });
 });
