54aab465bea7ea370e19c6c547404bb074329d37
Diego Pupato <pupato.diego@gmail.com>
2021-01-07 13:17:34 +1300

    refactor: move SUT creation to a factory method

--- src/presentation/pages/login/login.spec.tsx
+++ src/presentation/pages/login/login.spec.tsx
@@ -1,19 +1,33 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, RenderResult } from "@testing-library/react";
 import { IconBaseProps } from "react-icons";
 import { FaExclamationCircle } from "react-icons/fa";
 import Login from "./login";
 
+type SutTypes = {
+    sut: RenderResult;
+};
+
+const makeSut = (): SutTypes => {
+    const sut = render(<Login />);
+
+    return {
+        sut,
+    };
+};
+
 describe("Login Component", () => {
     test("Should start with initial state", () => {
-        const { getByTestId } = render(<Login />);
+        const { sut } = makeSut();
 
-        const errorWrap = getByTestId("error-wrap");
-        const submitButton = getByTestId("submit") as HTMLButtonElement;
-        const emailStatus = getByTestId("email-status");
-        const emailErrorIcon = getByTestId("email-error-icon") as IconBaseProps;
-        const passwordStatus = getByTestId("password-status");
-        const passwordErrorIcon = getByTestId(
+        const errorWrap = sut.getByTestId("error-wrap");
+        const submitButton = sut.getByTestId("submit") as HTMLButtonElement;
+        const emailStatus = sut.getByTestId("email-status");
+        const emailErrorIcon = sut.getByTestId(
+            "email-error-icon",
+        ) as IconBaseProps;
+        const passwordStatus = sut.getByTestId("password-status");
+        const passwordErrorIcon = sut.getByTestId(
             "password-error-icon",
         ) as IconBaseProps;
 
