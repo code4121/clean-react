b1a9a05bcfc3294aa6a7a6a955d81b558b43846a
Diego Pupato <pupato.diego@gmail.com>
2021-01-25 14:14:40 +1300

    test: ensure Login presents error if password is invalid

--- src/presentation/pages/login/login.spec.tsx
+++ src/presentation/pages/login/login.spec.tsx
@@ -9,23 +9,23 @@ import { IconBaseProps } from "react-icons";
 import { FaExclamationCircle } from "react-icons/fa";
 import faker from "faker";
 
-import { ValidationSpy } from "@/presentation/test";
+import { ValidationStub } from "@/presentation/test";
 
 import Login from "./login";
 
 type SutTypes = {
     sut: RenderResult;
-    validationSpy: ValidationSpy;
+    validationStub: ValidationStub;
 };
 
 const makeSut = (): SutTypes => {
-    const validationSpy = new ValidationSpy();
-    validationSpy.errorMessage = faker.random.words();
-    const sut = render(<Login validation={validationSpy} />);
+    const validationStub = new ValidationStub();
+    validationStub.errorMessage = faker.random.words();
+    const sut = render(<Login validation={validationStub} />);
 
     return {
         sut,
-        validationSpy,
+        validationStub,
     };
 };
 
@@ -33,7 +33,7 @@ describe("Login Component", () => {
     afterEach(cleanup);
 
     test("Should start with initial state", () => {
-        const { sut, validationSpy } = makeSut();
+        const { sut, validationStub } = makeSut();
 
         const errorWrap = sut.getByTestId("error-wrap");
         const submitButton = sut.getByTestId("submit") as HTMLButtonElement;
@@ -48,38 +48,14 @@ describe("Login Component", () => {
 
         expect(errorWrap.childElementCount).toBe(0);
         expect(submitButton.disabled).toBe(true);
-        expect(emailStatus.title).toBe(validationSpy.errorMessage);
+        expect(emailStatus.title).toBe(validationStub.errorMessage);
         expect(emailErrorIcon).toBeTruthy();
-        expect(passwordStatus.title).toBe("Required field");
+        expect(passwordStatus.title).toBe(validationStub.errorMessage);
         expect(passwordErrorIcon).toBeTruthy();
     });
 
-    test("Should call Validation with correct email", () => {
-        const { sut, validationSpy } = makeSut();
-        const email = faker.internet.email();
-
-        const emailInput = sut.getByTestId("email");
-
-        fireEvent.input(emailInput, { target: { value: email } });
-
-        expect(validationSpy.fieldName).toBe("email");
-        expect(validationSpy.fieldValue).toBe(email);
-    });
-
-    test("Should call Validation with correct password", () => {
-        const { sut, validationSpy } = makeSut();
-        const password = faker.internet.password();
-
-        const passwordInput = sut.getByTestId("password");
-
-        fireEvent.input(passwordInput, { target: { value: password } });
-
-        expect(validationSpy.fieldName).toBe("password");
-        expect(validationSpy.fieldValue).toBe(password);
-    });
-
     test("Should show email error if Validation fails", () => {
-        const { sut, validationSpy } = makeSut();
+        const { sut, validationStub } = makeSut();
         const emailInput = sut.getByTestId("email");
         const emailErrorIcon = sut.getByTestId(
             "email-error-icon",
@@ -90,8 +66,25 @@ describe("Login Component", () => {
         });
 
         const emailStatus = sut.getByTestId("email-status");
-        expect(emailStatus.title).toBe(validationSpy.errorMessage);
+        expect(emailStatus.title).toBe(validationStub.errorMessage);
         // expect(emailStatus.textContent).toBe(errorMessage);
         expect(emailErrorIcon).toBeTruthy();
+    });
+
+    test("Should show password error if Validation fails", () => {
+        const { sut, validationStub } = makeSut();
+        const passwordInput = sut.getByTestId("password");
+        const passwordErrorIcon = sut.getByTestId(
+            "password-error-icon",
+        ) as IconBaseProps;
+
+        fireEvent.input(passwordInput, {
+            target: { value: faker.internet.password() },
+        });
+
+        const passwordStatus = sut.getByTestId("password-status");
+        expect(passwordStatus.title).toBe(validationStub.errorMessage);
+        // expect(passwordStatus.textContent).toBe(errorMessage);
+        expect(passwordErrorIcon).toBeTruthy();
     });
 });
