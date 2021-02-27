19258b01b7f4b2f8e9fcdb981cfe8f36f65f5a53
Diego Pupato <pupato.diego@gmail.com>
2021-02-27 13:21:30 +1300

    test: prevent Login to call submit more than once

--- src/presentation/pages/login/login.spec.tsx
+++ src/presentation/pages/login/login.spec.tsx
@@ -188,4 +188,13 @@ describe("Login Component", () => {
             password,
         });
     });
+
+    test("Should call Authentication only once", () => {
+        const { sut, authenticationSpy } = makeSut();
+
+        simulateValidSubmit(sut);
+        simulateValidSubmit(sut);
+
+        expect(authenticationSpy.callsCount).toBe(1);
+    });
 });
