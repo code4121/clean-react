c98036b9bdd86e406c698c928bbf1d5f3944fdcb
Diego Pupato <pupato.diego@gmail.com>
2021-02-27 13:49:37 +1300

    feat: ensure Login add accessToken to localstorage on success

--- src/presentation/pages/login/login.tsx
+++ src/presentation/pages/login/login.tsx
@@ -49,10 +49,12 @@ const Login: React.FC<Props> = ({ validation, authentication }: Props) => {
 
             setState({ ...state, isLoading: true });
 
-            await authentication.auth({
+            const account = await authentication.auth({
                 email: state.email,
                 password: state.password,
             });
+
+            localStorage.setItem("@poll4devs:accessToken", account.accessToken);
         } catch (error) {
             setState({
                 ...state,
