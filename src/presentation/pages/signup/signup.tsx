c11053fc5d7fd4f83bbe88afc3b9c9b0f61f7c32
Diego Pupato <pupato.diego@gmail.com>
2021-03-03 19:18:00 +1300

    feat: ensure SignUp change email status to error if validation fails

--- src/presentation/pages/signup/signup.tsx
+++ src/presentation/pages/signup/signup.tsx
@@ -18,7 +18,8 @@ const SignUp: React.FC<Props> = ({ validation }: Props) => {
         isLoading: false,
         name: "",
         nameError: "",
-        emailError: "Field is required",
+        email: "",
+        emailError: "",
         passwordError: "Field is required",
         passwordConfirmationError: "Field is required",
         mainError: "",
@@ -27,9 +28,10 @@ const SignUp: React.FC<Props> = ({ validation }: Props) => {
     useEffect(() => {
         setState({
             ...state,
-            nameError: validation.validate("email", state.name),
+            nameError: validation.validate("name", state.name),
+            emailError: validation.validate("email", state.email),
         });
-    }, [state.name]);
+    }, [state.name, state.email]);
 
     return (
         <div className={Styles.signup}>
