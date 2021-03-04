9a624e558b19679ceb6baf7f7e1dac46884db06b
Diego Pupato <pupato.diego@gmail.com>
2021-03-04 17:06:24 +1300

    test: ensure SignUp presents error if SaveAccesstoken fails

--- src/presentation/pages/signup/signup.spec.tsx
+++ src/presentation/pages/signup/signup.spec.tsx
@@ -305,4 +305,17 @@ describe("SignUp Component", () => {
         expect(history.length).toBe(1);
         expect(history.location.pathname).toBe("/");
     });
+
+    // eslint-disable-next-line jest/expect-expect
+    test("Should present error if SaveAccessToken fails", async () => {
+        const { sut, saveAccessTokenMock } = makeSut();
+        const error = new EmailInUseError();
+
+        jest.spyOn(saveAccessTokenMock, "save").mockRejectedValueOnce(error);
+
+        await simulateValidSubmit(sut);
+
+        Helper.testChildCount(sut, "error-wrap", 1);
+        Helper.testElementText(sut, "main-error", error.message);
+    });
 });
